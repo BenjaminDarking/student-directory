dd500dfc23e4a00e8947200d11586d2d74a74d7c
Ben D <benjamin.d928@icloud.com>
2019-06-19 15:37:38 +0100

    Step 8 - indexing

--- directory.rb
+++ directory.rb
@@ -1,19 +1,4 @@
-student_count = 11
-puts "The students of Villains Academy"
-puts "---------------"
-students = [
-  {name: "Dr. hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -21,7 +6,7 @@ def input_students
   students = []
   name = gets.chomp
   #while the name is not empty, repeat this code
-  while :name.empty? do
+  while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     name = gets.chomp
@@ -36,8 +21,8 @@ puts "---------------"
 end
 
 def print(students)
-  students.each do |student|
-  puts "#{student[:name]} #{student[:cohort]} cohort"
+  students.each_with_index do |student, index|
+  puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort"
 end
 end
 
